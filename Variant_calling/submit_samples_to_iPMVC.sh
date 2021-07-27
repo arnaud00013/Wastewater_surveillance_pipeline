d860178172137721925bd12c6cfdb5bbfc4ac619
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2021-07-27 14:27:10 -0400

    Update submit_samples_to_iPMVC.sh

--- Variant_calling/submit_samples_to_iPMVC.sh
+++ Variant_calling/submit_samples_to_iPMVC.sh
@@ -5,6 +5,6 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=204800M
 #SBATCH --cpus-per-task=2
-module load  nixpkgs/16.09 gcc/7.3.0 intel/2018.3 gcc/7.3.0 blast+/2.9.0 prinseq/0.20.4 fastp/0.20.0 bwa/0.7.17 picard/2.20.6 samtools/1.10 varscan/2.4.1 python/3.6
+module load nixpkgs/16.09 gcc/7.3.0 intel/2018.3 gcc/7.3.0 blast+/2.9.0 prinseq/0.20.4 fastp/0.20.0 bwa/0.7.17 picard/2.20.6 samtools/1.10 varscan/2.4.1 python/3.6
 chmod u+x WORKSPACE/*.py WORKSPACE/*.sh
 ./run_iPMVC_in_parallel.py WORKSPACE lst_samples.txt 2
