a8297a58fe3196cf79db2a7ad80d43dfac5dcb69
arnaud00013 <45472731+arnaud00013@users.noreply.github.com>
2021-07-27 14:44:50 -0400

    Update submit_samples_to_iPMVC.sh

--- Variant_calling/submit_samples_to_iPMVC.sh
+++ Variant_calling/submit_samples_to_iPMVC.sh
@@ -8,3 +8,5 @@
 module load nixpkgs/16.09 gcc/7.3.0 intel/2018.3 gcc/7.3.0 blast+/2.9.0 prinseq/0.20.4 fastp/0.20.0 bwa/0.7.17 picard/2.20.6 samtools/1.10 varscan/2.4.1 python/3.6
 chmod u+x WORKSPACE/*.py WORKSPACE/*.sh
 ./run_iPMVC_in_parallel.py WORKSPACE lst_samples.txt 2
+#run the depth analysis
+sbatch submit_samples_to_cVC.sh
